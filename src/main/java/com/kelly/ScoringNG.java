ee0c3da7acbb5092847afe263b3eb3b958658dbb
Kelly <kellylan322@gmail.com>
2022-03-04 15:37:42 +0800

    ScoringNG

---
+++ src/main/java/com/kelly/ScoringNG.java
@@ -0,0 +1,13 @@
+package com.kelly;
+
+public class ScoringNG {
+    public static void main(String[] args) {
+        String[] names = {"Benny", "Jack", "Karen", "Hank", "Elene"};
+        int[] english = {70, 80, 65, 75, 90};
+        int[] maths = {40, 50, 70, 55, 35};
+        for (int i = 0; i < 5; i++) {
+            System.out.println(names[i] + "\t" + english[i] + "\t" + maths[i] + "\t" + (maths[i] + english[i] / 2));
+        }
+
+    }
+}
