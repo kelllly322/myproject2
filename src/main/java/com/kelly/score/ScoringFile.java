f9ff2f45b1bcfe435ed287d81f1190d6ed021402
kelllly322 <kellylan322@gmail.com>
2022-06-24 14:29:18 +0800

    202204019:50

---
+++ src/main/java/com/kelly/score/ScoringFile.java
@@ -0,0 +1,33 @@
+package com.kelly.score;
+
+import javax.sound.sampled.Line;
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class ScoringFile {
+    public static void main(String[] args) {
+        try {
+            FileReader fileReader = new FileReader("Scores.txt");//throws
+            BufferedReader reader = new BufferedReader(fileReader);
+            //Jack,85,35
+            String line = reader.readLine();
+            while (line != null) {
+                //        System.out.println(line);
+                String[] token = line.split(",");
+                String name = token[0];
+                int english = Integer.parseInt(token[1]);
+                int math = Integer.parseInt(token[2]);
+                Student stu = new Student(name, english, math);
+                stu.print();
+                line = reader.readLine();
+            }
+        } catch (FileNotFoundException e) {
+            System.out.println("檔案讀取失敗");
+        } catch (IOException e) {
+            System.out.println("資料讀取失敗");
+        }
+        System.out.println("Testing");
+    }
+}
