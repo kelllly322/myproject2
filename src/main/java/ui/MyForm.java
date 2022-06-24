23baf3811cdc360c147ebd7d8185a624e40e5272
kelllly322 <kellylan322@gmail.com>
2022-06-24 14:57:10 +0800

    ui guessing game

---
+++ src/main/java/ui/MyForm.java
@@ -0,0 +1,10 @@
+package ui;
+
+import javax.swing.*;
+
+public class MyForm {
+    private JButton helloButton;
+    private JPanel panel1;
+    private JTextField textFeild1;
+    private JCheckBox checkBox1;
+}
